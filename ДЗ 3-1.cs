f943273ef42d5686fb9e98864e1e8c241f43907c
AndresKaply <87811534+AndresKaply@users.noreply.github.com>
2021-08-01 21:43:24 +0300

    Add files via upload

---
+++ ДЗ 3-1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ДЗ_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mass = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j=0; j<mass.GetLength(1);j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write($"{mass[i,j]}");
+                    }
+
+                }
+                Console.WriteLine();
+
+            }
+
+
+        }
+    }
+}
