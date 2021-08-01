b5f2bc900863ebd67d20a09b4e901052269aa219
AndresKaply <87811534+AndresKaply@users.noreply.github.com>
2021-08-01 22:28:27 +0300

    Add files via upload

---
+++ ДЗ 3-3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ДЗ3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka = "Hello World!";
+            Console.WriteLine();
+
+            for (int i = stroka.Length - 1; i >= 0; i--)
+            {
+                Console.Write(stroka[i]);
+
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
